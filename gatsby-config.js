009523f7a091a07773dbc0732fb732c43b66c8f6
Santiago Chausis <santiagochausis@iMac-de-Santi.local>
2020-05-11 14:20:04 -0300

    Release #3

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ module.exports = {
       name: `Santi`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `Un blog de lectura!`,
+    siteUrl: `https://www.recomendadordelibros.com/`,
     social: {
       twitter: `kylemathews`,
     },
